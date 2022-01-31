068982fb37f42d2adb1e6cd5a57cda527724ebfa
Николай Пятнышков <n.piatnyshkov@egopay.ru>
2022-01-31 11:13:06 +0300

    add app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main_content">
+      Кредитная страница
+    </div>
+  );
+}
+
+export default App;
